435eaebe77baa4cc3df6c989de99b15edb94897e
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-12-14 13:16:48 +0700

    login dan register

--- ClientReservasi-062/Form1.cs
+++ ClientReservasi-062/Form1.cs
@@ -12,9 +12,95 @@ namespace ClientReservasi_062
 {
     public partial class Form1 : Form
     {
+        ServiceReferenceReservasi.Service1Client service = new ServiceReferenceReservasi.Service1Client();
         public Form1()
         {
             InitializeComponent();
+
+            TampilData();
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+            string NamaCustomer = textBoxNama.Text;
+            string NoTelepon = textBoxNoTlf.Text;
+            int JumlahPemesanan = int.Parse(textBoxJumlah.Text);
+            string IdLokasi = textBoxIDLokasi.Text;
+
+            var a = service.pemesanan(IDPemesanan, NamaCustomer, NoTelepon, JumlahPemesanan, IdLokasi);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+            string NamaCustomer = textBoxNama.Text;
+            string NoTelepon = textBoxNoTlf.Text;
+
+            var a = service.editPemesanan(IDPemesanan, NamaCustomer, NoTelepon);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+
+            var a = service.deletePemesanan(IDPemesanan);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+
+        }
+
+        public void TampilData()
+        {
+            var List = service.Pemesanan1();
+            dtPemesanan.DataSource = List;
+        }
+
+        public void Clear()
+        {
+            textBoxID.Clear();
+            textBoxNama.Clear();
+            textBoxNoTlf.Clear();
+            textBoxJumlah.Clear();
+            textBoxIDLokasi.Clear();
+
+            textBoxJumlah.Enabled = true;
+            textBoxIDLokasi.Enabled = true;
+
+            textBoxID.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dtPemesanan_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxID.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[0].Value);
+            textBoxNama.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[1].Value);
+            textBoxNoTlf.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[2].Value);
+            textBoxJumlah.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[3].Value);
+            textBoxIDLokasi.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[4].Value);
+
+            textBoxJumlah.Enabled = false;
+            textBoxIDLokasi.Enabled = false;
+
+            btnUpdate.Enabled = true;
+            btnHapus.Enabled = true;
+
+            btnSimpan.Enabled = false;
+            textBoxID.Enabled = false;
         }
     }
 }
---
+++ ClientReservasi-062/Register.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ClientReservasi_062
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.comboBoxKategori = new System.Windows.Forms.ComboBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dtRegister = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtRegister)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kategori";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(161, 59);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(205, 22);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(161, 150);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(205, 22);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(412, 12);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(247, 22);
+            this.textBoxID.TabIndex = 5;
+            // 
+            // comboBoxKategori
+            // 
+            this.comboBoxKategori.FormattingEnabled = true;
+            this.comboBoxKategori.Location = new System.Drawing.Point(161, 242);
+            this.comboBoxKategori.Name = "comboBoxKategori";
+            this.comboBoxKategori.Size = new System.Drawing.Size(205, 24);
+            this.comboBoxKategori.TabIndex = 6;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(498, 65);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 27);
+            this.btnSimpan.TabIndex = 7;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(498, 126);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 30);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(498, 188);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 27);
+            this.btnHapus.TabIndex = 9;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(498, 243);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dtRegister
+            // 
+            this.dtRegister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtRegister.Location = new System.Drawing.Point(54, 300);
+            this.dtRegister.Name = "dtRegister";
+            this.dtRegister.RowTemplate.Height = 24;
+            this.dtRegister.Size = new System.Drawing.Size(617, 196);
+            this.dtRegister.TabIndex = 11;
+            this.dtRegister.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtRegister_CellClick);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.dtRegister);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.comboBoxKategori);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.Load += new System.EventHandler(this.Register_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtRegister)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.ComboBox comboBoxKategori;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dtRegister;
+    }
+}
